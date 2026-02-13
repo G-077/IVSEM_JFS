7aa5f68c64b22a1abe4afa7660c0c05e51e9d9bc
G-077 <chilasagargkumar@gmail.com>
2026-02-13 11:35:22 +0530

    sink_nodes

---
+++ sink_nodes.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+
+
+    static int sink_nodes(int[][] arr) {
+        int sink_nodes = 0;
+        for(int i = 0; i < arr.length; i++){
+            int cnt = 0;
+            for(int j = 0; j < arr.length; j++){
+                if(arr[i][j] == 0){
+                    cnt++;
+                }
+            }
+            if(cnt == arr.length){
+                System.out.println("Sink Nodes: " + (char)(65+i));
+                sink_nodes++;
+            }
+        }
+        return sink_nodes;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] arr = {{0, 1, 1, 0, 1}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}};
+        System.out.println(sink_nodes(arr));
+    }
+
+}
